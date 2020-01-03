4c1eee7e3c7611f1102d466847896ddf6b363ec1
Marlon Lückert <m.lueckert@me.com>
2020-01-03 12:17:39 +0100

    fixed build config

--- webxr/webpack.prod.config.js
+++ webxr/webpack.prod.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const merge = require("webpack-merge");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
 const common = require("./webpack.common.config");
