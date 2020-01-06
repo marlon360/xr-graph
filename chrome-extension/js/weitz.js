f972c6acd5b34325e3697b652a74bdebf811f408
Marlon Lückert <m.lueckert@me.com>
2020-01-06 18:28:40 +0100

    weitz intervall scraping

--- chrome-extension/js/weitz.js
+++ chrome-extension/js/weitz.js
@@ -28,24 +28,44 @@ const tMax = findGetParameter("tmax");
 const aFactor = findGetParameter("afactor");
 const bFactor = findGetParameter("bfactor");
 
-xParam = xParam.replace('a', aFactor);
-xParam = xParam.replace('b', bFactor);
+xParam = xParam.replace(/a/g, aFactor);
+xParam = xParam.replace(/b/g, bFactor);
 
-yParam = yParam.replace('a', aFactor);
-yParam = yParam.replace('b', bFactor);
+yParam = yParam.replace(/a/g, aFactor);
+yParam = yParam.replace(/b/g, bFactor);
 
-zParam = zParam.replace('a', aFactor);
-zParam = zParam.replace('b', bFactor);
+zParam = zParam.replace(/a/g, aFactor);
+zParam = zParam.replace(/b/g, bFactor);
 
 let link = "https://vr-graph.now.sh?func=";
 if (tMin == null) {
-    link += "[" + [xParam, yParam, zParam].join(', ') + "]"
+    link += encodeURIComponent("[" + [xParam, yParam, zParam].join(', ') + "]")
+    if (uMin != null) {
+        link += "&xmin=" + uMin
+    }
+    if (uMax != null) {
+        link += "&xmax=" + uMax
+    }
+    if (vMin != null) {
+        link += "&zmin=" + vMin
+    }
+    if (vMax != null) {
+        link += "&zmax=" + vMax
+    }
 } else {
     link += "[" + [xParam, yParam, zParam].join(', ') + "]"
+    if (tMin != null) {
+        link += "&tmin=" + tMin
+    }
+    if (tMax != null) {
+        link += "&tmax=" + tMax
+    }
 }
+
 
 let button = document.createElement("a");
 button.innerText = "View in VR"
 button.id = "vr-button"
 button.href = link;
+button.target = "_blank"
 document.body.append(button);
