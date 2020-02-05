f1896ed2fdfc18c17f11d941ae6b883aec029c8d
Marlon Lückert <m.lueckert@me.com>
2020-02-05 17:14:38 +0100

    curve coloring

--- webxr/src/components/GraphComponent/MathCurveShader.js
+++ webxr/src/components/GraphComponent/MathCurveShader.js
@@ -69,6 +69,8 @@ export class MathCurveMaterial {
         
         // pass a few things along to the vertex shader
         varying vec2 vUv;
+        varying float parameter;
+        varying float parameterRange;
         varying vec3 vViewPosition;
         varying vec3 vNormal;
         
@@ -116,11 +118,13 @@ export class MathCurveMaterial {
           vec3 transformed;
           vec3 objectNormal;
           createTube(t, volume, transformed, objectNormal);
+          parameter = t;
         
           // pass the normal and UV along
           vec3 transformedNormal = normalMatrix * objectNormal;
           vNormal = normalize(transformedNormal);
           vUv = uv.yx; // swizzle this to match expectations
+          parameterRange = ${this.expression.getParameters()[0]}Range;
         
           // project our vertex position
           vec4 mvPosition = modelViewMatrix * vec4(transformed, 1.0);
@@ -132,8 +136,58 @@ export class MathCurveMaterial {
 
     fragmentShader() {
         return `
+        varying float parameter;
+        varying float parameterRange;
+
+        float HueToRGB(float f1, float f2, float hue) {
+            if (hue < 0.0)
+                hue += 1.0;
+            else if (hue > 1.0)
+                hue -= 1.0;
+            float res;
+            if ((6.0 * hue) < 1.0)
+                res = f1 + (f2 - f1) * 6.0 * hue;
+            else if ((2.0 * hue) < 1.0)
+                res = f2;
+            else if ((3.0 * hue) < 2.0)
+                res = f1 + (f2 - f1) * ((2.0 / 3.0) - hue) * 6.0;
+            else
+                res = f1;
+            return res;
+        }
+
+        vec3 HSLToRGB(vec3 hsl) {
+            vec3 rgb;
+
+            if (hsl.y == 0.0) {
+                rgb = vec3(hsl.z); // Luminance
+            }
+            else {
+                float f2;
+                
+                if (hsl.z < 0.5)
+                    f2 = hsl.z * (1.0 + hsl.y);
+                else
+                    f2 = (hsl.z + hsl.y) - (hsl.y * hsl.z);
+                
+                float f1 = 2.0 * hsl.z - f2;
+                
+                rgb.r = HueToRGB(f1, f2, hsl.x + (1.0/3.0));
+                rgb.g = HueToRGB(f1, f2, hsl.x);
+                rgb.b= HueToRGB(f1, f2, hsl.x - (1.0/3.0));
+            }
+            return rgb;
+        }
+
+        vec4 userDefinedColor() {
+            float h = 0.7 * parameter / parameterRange;
+            float s = 1.0;
+            float l = 0.5;
+            return vec4(HSLToRGB(vec3(h, s, l)), 1.0);
+        }
+
         void main() {
-            gl_FragColor = vec4(0.1,0.8,0.1,1.0);
+            gl_FragColor = userDefinedColor();
         }
         `
     }
