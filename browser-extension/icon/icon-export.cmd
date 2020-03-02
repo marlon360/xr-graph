4e3b5521fa16996589c9a6865673d4e4a05162b2
Marlon Lückert <m.lueckert@me.com>
2020-03-02 13:37:01 +0100

    popup for browser extension, cleanup

--- browser-extension/icon/icon-export.cmd
+++
@@ -1,1 +0,0 @@
-for %%x in (16 19 32 38 48 128) ; do "C:\Program Files (x86)\Inkscape\inkscape.exe" --export-png icon-%%x.png -w %%x icon.svg ; done
--- browser-extension/js/config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/*
- * Common variables and constants
- */
-const DEVELOPMENT_MODE = !('update_url' in chrome.runtime.getManifest());
-const DOMAINS_FORBIDDEN = [
-    'example.com',
-];
